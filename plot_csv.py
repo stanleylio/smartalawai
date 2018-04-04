3ef1f5138f9d3b0d5acf60b463c86e0998180717
Stanley Lio <stanleylio@gmail.com>
2018-04-04 02:13:57 +0000

    ,

--- plot_csv.py
+++ plot_csv.py
@@ -107,5 +107,5 @@ plt.grid(True)
 plt.gcf().autofmt_xdate()
 ax1.xaxis.set_major_formatter(DateFormatter('%b %d %H:%M:%S'))
 
-plt.savefig(UNIQUE_ID + '.png', dpi=600)
+plt.savefig(flash_id + '.png', dpi=600)
 plt.show()
